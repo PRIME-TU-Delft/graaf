7d8d55604663076d695f87513482aa7a877fdfc0
Bluerberry <boazkreulen.main@gmail.com>
2024-02-21 16:41:51 +0100

    card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,40 @@
+
+<!-- Script -->
+
+<script lang="ts">
+
+    export let header: string;
+
+</script>
+
+<!-- Markup -->
+
+<div class="card">
+    <div class="card-header">{header}</div>
+    <div class="card-body">
+        <slot />
+    </div>
+</div>
+
+<!-- Styles -->
+
+<style lang="sass">
+    
+    @use "$styles/variables.sass"
+
+    .card
+        border-radius: 0.25rem
+        border: 1px solid rgba(0, 0, 0, .125)
+
+        .card-header
+            margin: 0
+            padding: 0.75rem 1.25rem
+
+            background-color: variables.$light-gray
+            border-radius: calc(.25rem - 1px) calc(.25rem - 1px) 0 0
+            border-bottom: 1px solid rgba(0, 0, 0, .125)
+
+        .card-body
+            padding: 1.25rem
+
+</style>
---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,18 @@
+
+<!-- Script -->
+
+<script lang="ts">
+    
+    import Card from '$components/Card.svelte';
+
+</script>
+
+<!-- Markup -->
+
+<Card header="Login" />
+
+<!-- Styles -->
+
+<style lang="sass">
+
+</style>
