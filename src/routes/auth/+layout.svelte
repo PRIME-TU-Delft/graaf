28141c9ad3fb86a4b1005ea6200b376194e68882
Bluerberry <boazkreulen.main@gmail.com>
2024-03-06 11:49:59 +0100

    commit -a doesnt add untracked files

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,19 @@
+
+<!-- Markup -->
+
+<div>
+    <slot />
+</div>
+
+<!-- Styles -->
+
+<style lang="sass">
+
+    @use "$styles/variables.sass" as *
+    @use "$styles/palette.sass" as *
+
+    div
+        width: 100%
+        max-width: $small-column
+
+</style>
