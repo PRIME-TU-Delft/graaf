9e71b9847193eabede09ea1a44c6d19eebc5ea55
Bluerberry <boazkreulen.main@gmail.com>
2024-03-28 18:16:55 +0100

    Form component

---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,23 @@
+
+<script lang="ts">
+
+    import { enhance } from '$app/forms';
+
+    export let action: string = '';
+
+</script>
+
+<form method="POST" action={action} use:enhance>
+	<slot />
+</form>
+
+<style lang="sass">
+
+	form
+		display: flex
+		flex-flow: column nowrap
+		gap: 0.5em
+
+		width: 100%
+
+</style>
