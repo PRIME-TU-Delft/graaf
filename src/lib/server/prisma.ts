234b8b04fd2d0136c9ec7a8e5f6f794e36295c6a
Julia van der Kris <juliavdkris@proton.me>
2024-06-20 00:14:09 +0200

    Finish DomainExtension to convert Domain object to SerializedDomain DTO

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -2,10 +2,14 @@ import { PrismaClient } from '@prisma/client';
 
 import GraphExtension from '$lib/server/GraphExtension';
 import LectureExtension from '$lib/server/LectureExtension';
+import DomainExtension from '$lib/server/DomainExtension';
+// import SubjectExtension from '$lib/server/SubjectExtension';
 
 
 const prisma = new PrismaClient()
 	.$extends(GraphExtension)
-	.$extends(LectureExtension);
+	.$extends(LectureExtension)
+	.$extends(DomainExtension);
+	// .$extends(SubjectExtension);
 
 export default prisma;
