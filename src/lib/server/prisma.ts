634e12aa31ae9184f006f87ec41e520cb9a0ef07
Julia van der Kris <juliavdkris@proton.me>
2024-07-10 22:27:32 +0200

    Oopsie woopsie fucky wucky

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,12 +1,12 @@
 import { type PrismaClient } from '@prisma/client';
 import { createRequire } from 'module';
-import { DEBUG } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 const require = createRequire(import.meta.url);
 
 const { PrismaClient: RequiredPrismaClient } = require('@prisma/client');
 const prisma: PrismaClient = new RequiredPrismaClient({
-	log: DEBUG ? ['query', 'info', 'warn', 'error'] : ['error']
+	log: env.DEBUG ? ['query', 'info', 'warn', 'error'] : ['error']
 });
 
 export default prisma;
