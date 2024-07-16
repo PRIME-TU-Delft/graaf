d0302c0d9e006b96c3fdc1045ea32599b39812b2
Julia van der Kris <juliavdkris@proton.me>
2024-07-16 20:46:03 +0200

    Fix Auth.js config after hours of debugging and hopelessly scouring the internet
    Swear to god I'm gonna punch an Auth.js dev what the fuck is this

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,23 +1,19 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
-import { type Provider } from "@auth/sveltekit/providers";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import prisma from "$lib/server/prisma";
 import { env } from "$env/dynamic/private";
 
 
-const surfProvider: Provider = {
-	id: "surfconext",
-	name: "SURFconext",
-	type: "oidc",
-	issuer: env.SURFCONEXT_ISSUER,
-	clientId: env.SURFCONEXT_CLIENT_ID,
-	clientSecret: env.SURFCONEXT_CLIENT_SECRET,
-};
-
-
 export const { handle, signIn, signOut } = SvelteKitAuth({
-	providers: [surfProvider],
+	providers: [{
+		id: "surfconext",
+		name: "SURFconext",
+		type: "oidc",
+		issuer: env.SURFCONEXT_ISSUER,
+		clientId: env.SURFCONEXT_CLIENT_ID,
+		clientSecret: env.SURFCONEXT_CLIENT_SECRET,
+	}],
 	adapter: PrismaAdapter(prisma),
 	secret: env.AUTH_SECRET,
 	// trustHost: true
