ae492e7871b4c7db7766f7b12f1fd988be9494cb
Julia van der Kris <juliavdkris@proton.me>
2024-11-20 15:34:03 +0100

    Use modern Sass backend
    Fixes "The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0" warning

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			sass: {
+				api: 'modern-compiler'
+			}
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
