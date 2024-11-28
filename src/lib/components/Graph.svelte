a5c0ec4cf9f4a9c15aa5706873fe5c4d3bdf46b8
Bram Kreulen <boazkreulen.main@gmail.com>
2024-11-28 14:11:08 +0100

    Domain legend

--- src/lib/components/Graph.svelte
+++ src/lib/components/Graph.svelte
@@ -8,8 +8,10 @@
 	import zoom_out_icon from '$assets/zoom-out-icon.svg'
 	import zoom_in_icon from '$assets/zoom-in-icon.svg'
 
-	// Exports
+	// Main
 	export let graphSVG: GraphSVG
+
+	let show_legend = false
 
 </script>
 
@@ -17,6 +19,15 @@
 
 <div class="graph">
 	<svg use:graphSVG.attach />
+
+	<button class="legend" class:show={show_legend} on:click={ () => show_legend = !show_legend }>
+		<h4 class="title"> Domain Legend </h4>
+		{#if show_legend}
+			{#each graphSVG.graph.domains as domain}
+				<span> {domain.name} <div class="preview" style:background={domain.color}/> </span>
+			{/each}
+		{/if}
+	</button>
 
 	<div class="zoom">
 		<button on:click={() => graphSVG.zoomIn()}><img src={zoom_in_icon} alt="Zoom in"></button>
@@ -28,6 +39,7 @@
 
 <style lang="sass">
 
+	@use "sass:math"
 	@use "$styles/variables.sass" as *
 	@use "$styles/palette.sass" as *
 
@@ -40,6 +52,61 @@
 			display: block
 			height: 100%
 			width: 100%
+
+		.legend
+			position: absolute
+			top: $card-thin-padding
+			right: $card-thin-padding
+
+			display: flex
+			flex-flow: column nowrap
+			padding: $card-thin-padding
+			align-items: flex-end
+
+			background: $white
+			border: 1px solid $gray
+			border-radius: $border-radius
+
+			cursor: pointer
+
+			$dropdown-icon-size: 0.75rem
+
+			& > *
+				pointer-events: none
+
+			&::after
+				content: ''
+
+				position: absolute
+				translate: -50% -50%
+				rotate: 45deg
+				top: 0.75rem + $card-thin-padding + math.div($dropdown-icon-size, 4)
+				right: $card-thin-padding
+
+				width: math.div($dropdown-icon-size, math.sqrt(2))
+				height: math.div($dropdown-icon-size, math.sqrt(2))
+
+				border: 1px solid $dark-gray
+				border-width: 1px 0 0 1px
+			
+			&.show::after
+				top: 0.75rem + $card-thin-padding - math.div($dropdown-icon-size, 4)
+				border-width: 0 1px 1px 0
+
+			.title
+				padding-right: $card-thin-padding + 1rem
+
+			span
+				display: flex
+				flex-flow: row nowrap
+				align-items: center
+
+				color: $dark-gray
+
+				.preview
+					width: 1rem
+					height: 1rem
+					margin-left: $form-small-gap
 
 		.zoom
 			position: absolute
