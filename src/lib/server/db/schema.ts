87dffd19b5331908aacb7a09d6e239d07d45eb2c
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-12-20 17:01:31 +0100

    Define schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,17 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
+export const program = pgTable('program', {
 	id: serial('id').primaryKey(),
-	age: integer('age')
+	name: text('name').notNull(),
+	description: text('description').notNull()
+});
+
+export const course = pgTable('course', {
+	id: serial('id').primaryKey(),
+	name: text('name').notNull(),
+	type: text('type'),
+	description: text('description').notNull(),
+	programId: serial('program_id')
+		.notNull()
+		.references(() => program.id)
 });
