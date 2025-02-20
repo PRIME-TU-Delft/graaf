f6568fe8e278c2debda9bec1fa5985b068eac2e7
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-02-20 22:13:47 +0100

    Added rudamentary auth

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,68 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+import { env } from '$env/dynamic/private';
+import type { OIDCConfig } from '@auth/sveltekit/providers';
+import prisma from './db/prisma';
+import { error } from '@sveltejs/kit';
+
+interface SurfConextProfile extends Record<string, any> {
+	nickname: string;
+	firstName: string;
+	lastName: string;
+	email: string;
+}
+
+/**
+ * Workaround for Auth.js not automatically fetching userinfo through endpoint specified in .well-known config
+ *
+ * @param accessToken JWT access token
+ * @returns Object containing user information
+ */
+async function fetchUserInfo(accessToken: string | undefined) {
+	if (!accessToken) throw error(505, 'No access token provided');
+	const res = await fetch(`${env.SURFCONEXT_ISSUER}/oidc/userinfo`, {
+		headers: {
+			Authorization: `Bearer ${accessToken}`
+		}
+	});
+	return await res.json();
+}
+
+function SurfConextProvider<P extends SurfConextProfile>(): OIDCConfig<P> {
+	return {
+		id: 'surfconext',
+		name: 'SURFconext',
+		type: 'oidc',
+		issuer: env.SURFCONEXT_ISSUER,
+		wellKnown: `${env.SURFCONEXT_ISSUER}/.well-known/openid-configuration`,
+		clientId: env.SURFCONEXT_CLIENT_ID,
+		clientSecret: env.SURFCONEXT_CLIENT_SECRET,
+
+		async profile(profile, tokens): Promise<SurfConextProfile> {
+			const userInfo = await fetchUserInfo(tokens.access_token);
+			const res = {
+				nickname: userInfo.nickname,
+				firstName: userInfo.given_name,
+				lastName: userInfo.family_name,
+				email: userInfo.email
+			};
+			return res;
+		}
+	};
+}
+
+export const { handle, signIn, signOut } = SvelteKitAuth({
+	providers: [SurfConextProvider],
+	adapter: PrismaAdapter(prisma),
+	secret: env.AUTH_SECRET,
+	debug: Boolean(env.DEBUG),
+	trustHost: true,
+
+	callbacks: {
+		// ¯\_(ツ)_/¯
+		session({ session }) {
+			return session;
+		}
+	}
+});
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { page } from '$app/state';
+	import { SignIn, SignOut } from '@auth/sveltekit/components';
+</script>
+
+<div class="prose">
+	<h1>SvelteKit Auth Example</h1>
+	<div>
+		{#if page.data.session}
+			{#if page.data.session.user?.image}
+				<img src={page.data.session.user.image} class="avatar" alt="User Avatar" />
+			{/if}
+			<span class="signedInText">
+				<small>Signed in as</small><br />
+
+				<pre>{JSON.stringify(page.data.session, null, 2)}</pre>
+
+				<strong>{page.data.session.user?.name ?? 'User'}</strong>
+			</span>
+			<SignOut>
+				<div slot="submitButton" class="buttonPrimary">Sign out</div>
+			</SignOut>
+		{:else}
+			<span class="notSignedInText">You are not signed in</span>
+			<SignIn>
+				<div slot="submitButton" class="buttonPrimary">Sign in</div>
+			</SignIn>
+		{/if}
+	</div>
+</div>
