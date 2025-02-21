843c7c8c588d80063561f26644af334d694f7d04
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-02-21 10:58:35 +0100

    refactor: change import to use type for PageData in settings page

--- src/routes/graph-editor/programs/[uuid]/settings/+page.svelte
+++ src/routes/graph-editor/programs/[uuid]/settings/+page.svelte
@@ -2,7 +2,7 @@
 	import { enhance } from '$app/forms';
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
 	import { buttonVariants } from '$lib/components/ui/button/index.js';
-	import { PageData } from './$types';
+	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
 </script>
