84d1b8e1f4a45740fcf7e4aa9dff5d6acf4469be
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-02-26 15:56:10 +0100

    refactor: simplify user retrieval logic in deploy previews

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,11 +8,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 	// Disable all forms of authentication in deploy previews
 	if (env.NETLIFY_CONTEXT == 'DEPLOY_PREVIEW') {
 		event.locals.auth = async () => {
-			const user = await prisma.user.findFirst({
-				where: {
-					email: 'testuser@tudelft.nl'
-				}
-			});
+			const user = await prisma.user.findFirst();
 
 			if (!user) throw new Error('User not found');
 
