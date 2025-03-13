a3a34854dae1aa2309b3dbe8d788d2bf5c79435d
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-03-13 16:22:36 +0100

    fix: update import type for graphid parameter in graph editor

--- src/routes/graph-editor/courses/[code]/graphs/+page.server.ts
+++ src/routes/graph-editor/courses/[code]/graphs/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './[graphid]/$types';
+import type { LayoutServerLoad } from './[graphid=int]/$types';
 
 export const load: LayoutServerLoad = async ({ params }) => {
 	redirect(303, `/graph-editor/courses/${params.code}`);
