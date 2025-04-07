73cdd6f56118e252737ef871218941f7af3acf18
Julia van der Kris <juliavdkris@proton.me>
2025-04-07 18:10:28 +0200

    Make prettier & eslint shut up :)

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: process.env.BUILD_ENV === 'netlify' ? netlifyAdapter() : nodeAdapter(),
+		adapter: process.env.BUILD_ENV === 'netlify' ? netlifyAdapter() : nodeAdapter()
 	}
 };
 
