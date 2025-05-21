9f6a9b43f899bf56eac54393b0ccf7f7d4713dfb
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-05-21 10:35:12 +0200

    fix: update MAX_COURSE_CODE_LENGTH to 30

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -9,7 +9,7 @@ export const MAX_PROGRAM_NAME_LENGTH = 50;
 
 // Course settings
 export const COURSE_CODE_REGEX = /^[A-Za-z0-9]*$/;
-export const MAX_COURSE_CODE_LENGTH = 255;
+export const MAX_COURSE_CODE_LENGTH = 30;
 export const MAX_COURSE_NAME_LENGTH = 50;
 
 // Graph settings
