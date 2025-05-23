6cc539803f6b36e4bd4b1bffaaa99e0c044eb08f
Abel de Bruijn <abeldebruijn@hotmail.com>
2025-05-23 14:55:46 +0200

    Add editable prop to GraphDecorators and update GraphRenderer usage

--- src/routes/graph/[code]/[alias]/+page.server.ts
+++ src/routes/graph/[code]/[alias]/+page.server.ts
@@ -1,4 +1,5 @@
-import type { ServerLoad } from '@sveltejs/kit';
+import prisma from '$lib/server/db/prisma';
+import { error, type ServerLoad } from '@sveltejs/kit';
 
 export const load: ServerLoad = async ({ params }) => {
 	const courseCode = params.code;
@@ -8,8 +9,53 @@ export const load: ServerLoad = async ({ params }) => {
 		throw new Error('Course code and alias are required');
 	}
 
-	return {
-		courseCode,
-		alias
-	};
+	try {
+		const graph = await prisma.graph.findFirst({
+			where: {
+				course: {
+					code: courseCode
+				},
+				links: {
+					some: {
+						name: alias
+					}
+				}
+			},
+			include: {
+				domains: {
+					include: {
+						sourceDomains: true,
+						targetDomains: true
+					},
+					orderBy: {
+						order: 'asc'
+					}
+				},
+				subjects: {
+					include: {
+						sourceSubjects: true,
+						targetSubjects: true,
+						domain: true
+					},
+					orderBy: {
+						order: 'asc'
+					}
+				},
+				lectures: {
+					include: {
+						subjects: true
+					}
+				}
+			}
+		});
+
+		if (!graph) error(404, { message: 'Graph not found' });
+
+		// Happy path
+		return {
+			graph: graph
+		};
+	} catch (e: unknown) {
+		error(500, { message: e instanceof Error ? e.message : `${e}` });
+	}
 };
--- src/routes/graph/[code]/[alias]/+page.svelte
+++ src/routes/graph/[code]/[alias]/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+	import GraphRenderer from '$lib/components/GraphRenderer.svelte';
+
 	let { data } = $props();
 </script>
 
-This is {data.courseCode} graph {data.alias}
+<div class="sticky h-[calc(100dvh)] w-full rounded-lg bg-purple-200/50 p-2">
+	<GraphRenderer data={data.graph} editable={false} />
+</div>
